796aecc0bdcd24a00a80da45388e688be1d487cf
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-01-13 19:23:35 -0300

    adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
+import Album from "./pages/Album";
+import Favorites from "./pages/Favorites";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import Profile from "./pages/Profile";
+import ProfileEdit from "./pages/ProfileEdit";
+import Search from "./pages/Search";
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <main>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ Login } />
+            <Route path="/search" component={ Search } />
+            <Route 
+              path="/album/:id"
+              render={ (propsRoute) => (<Album { ...propsRoute } />) }
+            />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/profile" component={ Profile } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </main>
+    );
   }
 }
 
---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div data-testid="page-not-found">
+        <h1>404 Página não encontrada</h1>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
