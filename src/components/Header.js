d10c0a7e5957e2883c09792b84b9df108f5df7d7
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-01-15 10:22:39 -0300

    adiciona requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
   render() {
-    return <header data-testid="header-component">Header</header>;
+    return (
+      <header data-testid="header-component">
+        <nav>
+          <ul>
+            <Link data-testid="link-to-search" to="/search">
+              Search
+            </Link>
+            <Link data-testid="link-to-favorites" to="/favorites">
+              Favorites
+            </Link>
+            <Link data-testid="link-to-profile" to="/profile">
+              Profile
+            </Link>
+          </ul>
+        </nav>
+      </header>
+    );
   }
 }
 
