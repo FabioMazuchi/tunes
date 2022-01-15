bcc5ab5646dbe7b800cdbbbe134bd3318a8d7b31
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-01-15 10:15:07 -0300

    adiciona requisito 3

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,13 +1,15 @@
-import { Component } from 'react';
+import React, { Component } from "react";
+import Header from "../components/Header";
 
 class Album extends Component {
   render() {
     return (
       <div data-testid="page-album">
+        <Header />
         <h1>Album: </h1>
       </div>
     );
   }
 }
 
-export default Album
+export default Album;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,13 +1,42 @@
-import { Component } from 'react';
+import React, { Component } from "react";
+import Header from "../components/Header";
+import { getUser } from "../services/userAPI";
+import Loading from '../components/Loading';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: "",
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.renderUser();
+  }
+
+  async renderUser() {
+    const obj = await getUser();
+    const nome = obj.name;
+    this.setState({ 
+      userName: nome,
+      loading: false,
+    });
+    console.log(obj.name);
+  }
+
   render() {
+    const { userName, loading } = this.state;
     return (
       <div data-testid="page-search">
-        <h1>Search</h1>
+        <Header />
+        {loading ? <Loading /> : (
+          <h2 data-testid="header-user-name">{userName}</h2>
+        )}
       </div>
     );
   }
 }
 
-export default Search
+export default Search;
