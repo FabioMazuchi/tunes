85cb13939edb20d988371630f538264d855b52a5
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-01-15 10:47:24 -0300

    adiciona requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import { getUser } from "../services/userAPI";
-import Loading from '../components/Loading';
+import Loading from "../components/Loading";
+
+const MIN_NOME = 2;
 
 class Search extends Component {
   constructor() {
@@ -9,31 +11,70 @@ class Search extends Component {
     this.state = {
       userName: "",
       loading: true,
+      artistName: "",
+      isDisabled: true,
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.validateinputs = this.validateinputs.bind(this);
   }
 
   componentDidMount() {
     this.renderUser();
   }
 
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, () => this.validateinputs());
+  }
+
+  validateinputs() {
+    const { artistName } = this.state;
+
+    if (artistName.length >= MIN_NOME) {
+      this.setState({ isDisabled: false });
+    } else {
+      this.setState({ isDisabled: true });
+    }
+  }
+
   async renderUser() {
     const obj = await getUser();
     const nome = obj.name;
-    this.setState({ 
+    this.setState({
       userName: nome,
       loading: false,
     });
-    console.log(obj.name);
   }
 
   render() {
-    const { userName, loading } = this.state;
+    const { userName, loading, artistName, isDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        {loading ? <Loading /> : (
+        {loading ? (
+          <Loading />
+        ) : (
           <h2 data-testid="header-user-name">{userName}</h2>
         )}
+        <form>
+          <input
+            name="artistName"
+            data-testid="search-artist-input"
+            type="text"
+            placeholder="Pesquisar artista..."
+            onChange={this.handleChange}
+            value={artistName}
+          />
+          <button
+            disabled={isDisabled}
+            data-testid="search-artist-button"
+            type="submit"
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
