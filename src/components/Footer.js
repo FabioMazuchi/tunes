626a413a6129fcdba0cd990af1f86b4194624cbf
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-02-24 08:58:19 -0300

    pre reafactor

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>Desenvolvido por Fábio Augusto Mazuchi</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
