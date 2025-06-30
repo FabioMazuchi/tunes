7f80f8e6c735492d1f7d7d9c1cdc93be12196a7f
biozuchi <mazuchi.augusto@hotmail.com>
2025-06-30 08:44:16 -0300

    update data

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import { FooterStl } from './styles';
 export default function Footer() {
   return (
     <FooterStl>
-      <p>Todos os direitos reservados - Tunes © 2025</p>
+      <p>Todos os direitos reservados | Tunes © 2025</p>
     </FooterStl>
   );
 }
